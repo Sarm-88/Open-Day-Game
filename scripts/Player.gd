adf1ca1f372840571dc313bec5b6a8e7091088c1
Sarm-88 <diesel1840@gmail.com>
2022-11-22 11:01:46 +0000

    Fixed Camera Move

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -10,12 +10,15 @@ onready var gun = $Gun
 onready var gun_sprite = $Gun/Sprite
 onready var end_of_barrell = $Gun/Sprite/EndOfBarrell
 
+onready var label = $Label
+
 
 var bullet = preload("res://scenes/Bullet.tscn")
 
 
 var speed = 500
 var vel = Vector2.ZERO
+var mouse = Vector2.ZERO
 
 
 func _physics_process(delta):
@@ -24,21 +27,32 @@ func _physics_process(delta):
 	vel.y = Input.get_action_strength("down") - Input.get_action_strength("up")
 	move_and_slide(vel.normalized() * speed)
 	
+	# Gun Aim
+	mouse = get_global_mouse_position()
+	gun.look_at(mouse)
+
+
+func _process(delta):
+	# Gun Flip
+	if mouse.x < position.x:
+		gun_sprite.flip_v = true
+	else:
+		gun_sprite.flip_v = false
+	
 	# Player Animation
 	if vel:
 		anim_player.play("walk")
 	else:
 		anim_player.play("RESET")
 	
-	# Gun Aim
-	var mouse = get_global_mouse_position()
-	gun.look_at(mouse)
+	# Camera Move
+	var camera_offset = (mouse - self.position).normalized()
+	print(camera_offset)
+	if mouse.distance_to(self.position) < 300:
+		camera_offset = Vector2(0, 0)
 	
-	if mouse.x < position.x:
-		gun_sprite.flip_v = true
-	else:
-		gun_sprite.flip_v = false
-		
+	camera.position = Vector2(camera_offset.x * 30, camera_offset.y * 30)
+
 
 func _unhandled_input(event):
 	if Input.is_action_just_pressed("quit"):
--- scripts/Root.gd
+++ scripts/Root.gd
@@ -2,6 +2,10 @@ extends Node2D
 
 
 onready var grass_holder = $GrassHolder
+onready var label = $Label
+onready var player = $Player
+
+
 var grass = preload("res://scenes/Grass.tscn")
 
 var grass_amount = 10
@@ -15,3 +19,14 @@ func _ready():
 		var grass_instance = grass.instance()
 		grass_instance.position = pos
 		grass_holder.add_child(grass_instance)
+
+
+func _process(delta):
+	var mouse = get_local_mouse_position()
+	var mouse_offset = mouse - player.position
+	label.rect_position = Vector2(int(mouse.x), int(mouse.y)) + Vector2(50, 50)
+	
+	if mouse.distance_to(player.position) < 300:
+		label.text = "bingus"
+	else:
+		label.text = String(mouse_offset.x) + ", " + String(mouse_offset.y)
