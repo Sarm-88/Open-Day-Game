c9b598e996bb911b0d69e0e62f8a47fcba1e20be
Sarm-88 <diesel1840@gmail.com>
2022-11-22 18:05:22 +0000

    fuck

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -14,6 +14,7 @@ onready var gun_sprite = $Gun/Sprite
 onready var end_of_barrell = $Gun/EndOfBarrell
 
 onready var shoot_timer = $ShootTimer
+onready var shoot_sound = $AudioStreamPlayer
 
 
 var bullet = preload("res://scenes/Bullet.tscn")
@@ -56,7 +57,7 @@ func _process(delta):
 	if mouse.distance_to(self.position) < 300: # Move camera to center if close to player
 		camera_offset = Vector2(0, 0)
 	
-	camera.position = Vector2(camera_offset.x * 30, camera_offset.y * 30)
+	camera.position = Vector2(camera_offset.x * 20, camera_offset.y * 20)
 	
 	# Camera Shake
 	if shake_amount > 0:
@@ -72,7 +73,8 @@ func _unhandled_input(event):
 	
 	if Input.is_action_just_pressed("shoot"):
 		shake_amount = 5
-		
+			
 		if shoot_timer.is_stopped():
 			emit_signal("shoot", end_of_barrell)
 			shoot_timer.start()
+			shoot_sound.play()
